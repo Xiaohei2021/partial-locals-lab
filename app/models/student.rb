c1f550683365ec22d59365b923a999119f21296a
Leon <hualiang.tian@gmail.com>
2021-07-29 16:26:46 -0400

    commit tree 3a2c6cc1ff9d51e900b2d45e1d75460a08cd464b

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  def self.search(query)
+    # binding.pry
+    if query.present?
+      self.where("NAME like ?", "%#{query}%")
+    else
+      self.all
+    end
+  end
+
 end
